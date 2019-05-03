efda5fa9803e3a934f5bac55d144f02cedaeaada
Noge Vallès <nogeh94@gmail.com>
2019-05-03 19:41:00 +0200

    Basic Level Generator

---
+++ VallesgameJam/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+	[SerializeField] private GameObject ufoPrefab;
+	[SerializeField] private Transform playerTransform;
+
+	public float nextSpawnY;
+	
+	const float SEPARATION_BETWEEN_OVNIES = 2f;
+	const float MAX_X_POSITION = 5.5f;
+	
+	void Start ()
+	{
+
+		UpdateUfos();
+		
+	}
+
+	private void Update()
+	{
+		UpdateUfos();
+	}
+	
+	private void UpdateUfos()
+	{
+		if (playerTransform.position.y + 10f < nextSpawnY)
+		{
+			return;
+		}
+
+		SpawnUfo(nextSpawnY);
+		nextSpawnY += SEPARATION_BETWEEN_OVNIES;
+		
+	}
+
+	private void SpawnUfo(float positionY)
+	{
+		Instantiate(
+			ufoPrefab, 
+			new Vector3(
+				Random.Range(-MAX_X_POSITION, MAX_X_POSITION),
+				positionY,
+				0),
+			Quaternion.identity);
+	}
+
+}
